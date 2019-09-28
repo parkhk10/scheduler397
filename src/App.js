df970ba3d22b82f6ccd7dcaf23e233e63786795c
Keren Park <parkhk10@gmail.com>
2019-09-28 14:23:13 -0500

    implement slice 5: filtering by quarter and by conflicts

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React, {useState, useEffect} from 'react';
 import 'rbx/index.css';
 import {Button, Container, Title} from 'rbx';
 
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+// a conflict must involve overlapping days and times
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2 
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
+
 const Banner = ({title}) => (
   <Title>{title || '[loding...]'}</Title>
 );
-
-const CourseList = ({courses}) => (
-  <Button.Group>
-    {courses.map(course => <Course key={course.id} course={course} />)}
-  </Button.Group>
-);
-
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
@@ -22,11 +43,76 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-const Course = ({course}) => (
-  <Button>
-    {getCourseTerm(course)} CS {getCourseNumber(course)}
+const buttonColor = selected => (
+  selected ? 'success' : null
+);
+
+const TermSelector = ({state}) => (
+  <Button.Group hasAddons>
+    { Object.values(terms)
+        .map(value => <Button key={value}
+                        color={ buttonColor(value === state.term) }
+                        onClick={() => state.setTerm(value)}>{ value }</Button>
+        )
+    }
+  </Button.Group>
+);
+
+const Course = ({course, state}) => (
+  <Button color={buttonColor(state.selected.includes(course))}
+          onClick={() => state.toggle(course)}
+          disabled={hasConflict(course, state.selected)}
+  >
+    {getCourseTerm(course)} CS {getCourseNumber(course)} : {course.title}
   </Button>
 )
+
+// for toggling - like useState function but returns our own 
+// toggle function instead of setSelected function
+// essentially a custom hook
+const useSelection = () => {
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
+  };
+  return [selected, toggle];
+}
+
+const CourseList = ({courses}) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
+  const termCourses = courses.filter(course => term === getCourseTerm(course))
+  return (
+    <React.Fragment>
+    <TermSelector state={{term, setTerm}} />
+    <Button.Group>
+      {termCourses.map(course => <Course key={course.id} course={course} 
+                                    state={{selected, toggle}}/>)}
+    </Button.Group>
+  </React.Fragment>
+  )
+};
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course, // get all the previous attributes of a course and add a new attribute
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
 
 const App = () =>  {
   const [schedule, setSchedule] = useState({title: '', courses: []});
@@ -37,10 +123,10 @@ const App = () =>  {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
-  }, [])
+  }, []) // second argument [] makes it so that useEffect is run only when app is mounted
 
   return(
     <Container>
